8597c3d266abc6333c1adf20b40cb7fe3a3189ca
miltondw <estradamilton2001@gmail.com>
2021-04-08 19:33:59 -0500

    componente navigation parte basica terminada con estilos incluidos

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -5,6 +5,7 @@ import Navigation from "./components/Navigation";
 import NoteList from "./components/NoteList";
 import CreateUser from "./components/CreateUser";
 import CreateNote from "./components/CreateNote";
+import './styles/TodoList.scss'
 class TodoList extends Component {
   render() {
     return (
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Navigation extends Component {
     render() {
         return (
-            <div>
-                Navigation
-            </div>
+            <header>
+                <div className="logo">
+                    <NavLink to="/" exact >Notes app </NavLink>
+                </div>
+                <nav className="nav">
+                    <ul className="nav__ul">
+                        <li className="nav__li">   
+                            <NavLink to="/" exact  activeClassName="link__active" >Home</NavLink>
+                            <NavLink to="/user" activeClassName="link__active" >User</NavLink>
+                            <NavLink to="/create" activeClassName="link__active">Create Note</NavLink>
+                            <NavLink to="/edit/:id" activeClassName="link__active">Edit Note</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         );
     }
 }
