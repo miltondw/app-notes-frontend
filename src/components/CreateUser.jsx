a81a4a08aff897995b849d5d765c1cdc2c254281
miltondw <estradamilton2001@gmail.com>
2021-04-08 22:48:31 -0500

    metodos para crear y eliminar creados con sus respectivos estilos

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -8,15 +8,32 @@ class CreateUser extends Component {
     }
 
   async componentDidMount(){
-       const res= await axios.get('http://localhost:4000/api/users')
-       this.setState({
-           users:res.data
-       })
+       this.getUsers()
     }
-    onChangeUsername = (e)=>{
+     getUsers= async ()=>{
+        const res= await axios.get('http://localhost:4000/api/users')
+        this.setState({
+            users:res.data
+        })
+    }
+    onChangeUsername =e=>{
         this.setState({
             username:e.target.value
         })
+    }
+   onSubmit =async e=>{
+       e.preventDefault()
+       await axios.post('http://localhost:4000/api/users',{
+           username:this.state.username
+        })
+        this.setState({
+            username:''
+        })
+        this.getUsers()
+    }
+    deleteuser = async id=>{
+       await axios.delete(`http://localhost:4000/api/users/${id}`)
+       this.getUsers()
     }
     render() {
         return (
@@ -24,24 +41,34 @@ class CreateUser extends Component {
                <div className="form">
                    <div className="form__card">
                        <h2 className="form__title">Create new user</h2>
-                       <form>
+                       <form onSubmit={this.onSubmit}>
                                <input 
                                type="text" 
+                               value={this.state.username}
                                onChange={this.onChangeUsername}
                                placeholder="Username"
                                />
+                            <button type="submit" className="btn btn-user" >Save</button>
                        </form>
+
                    </div>
                </div>
                <ul className="list_users">
                    {
                        this.state.users.map(user=>(
-                           <li className="list_users__item" key={user._id}>
+                           <li 
+                                className="list_users__item" 
+                                key={user._id}
+                                >
                                <p className="list_users__name">
                                    {
                                        user.username
                                    }
                                </p>
+                               <i 
+                               className="btn_delete"
+                               onClick={()=>this.deleteuser(user._id)}
+                               >x</i>
                            </li>
                        ))
                    }
